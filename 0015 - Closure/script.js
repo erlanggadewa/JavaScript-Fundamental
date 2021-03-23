4b0230cd332a6b44b672a594e21cd475669d23a2
ErlanggaDewa <erlanggadewa7@gmail.com>
2021-03-23 16:02:01 +0700

    update

--- 0015 - Closure/script.js
+++ 0015 - Closure/script.js
@@ -1,6 +1,40 @@
-function satu() {
-  console.log(`halo ${nama}`);
+// //  * Closure karena console.log(nama) mengakeses variabel nama yang ada di lexical scope
+// // function test() {
+// //   let nama = "erlangga";
+// //   init();
+// //   function init() {
+// //     function tampilNama() {
+// //       console.log(nama);
+// //     }
+// //     tampilNama();
+// //     console.dir(tampilNama); // ! bisa dilihat di object scopes
+// //   }
+// // }
+// // test();
+
+// // * Contoh function factory
+// // function test() {
+// //   return init;
+// //   function init(nama) {
+// //     function tampilNama() {
+// //       console.log(nama);
+// //     }
+// //     tampilNama();
+// //     console.dir(tampilNama); // ! bisa dilihat di object scopes
+// //   }
+// // }
+// // let a = test();
+// // a("erlangga");
+
+// // * Contoh private method
+function salam(waktu) {
+  return function (nama) {
+    console.log(`Halo ${nama}, selamat ${waktu} ....`);
+  };
 }
-satu();
-// Jika kita menggunakan let maka akan error
-var nama = "wkwk";
+let salamPagi = salam("pagi");
+let salamSiang = salam("siang");
+let salamMalam = salam("malam");
+
+salamPagi("erlangga");
+// console.log(this.salamPagi("nada"));
