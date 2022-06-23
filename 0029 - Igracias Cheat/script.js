23fda563a92d48b3e14043023b72dc680d0d42d7
Erlangga Dewa Sakti <erlanggadewa7@gmail.com>
2022-06-23 17:55:35 +0700

    add function for quisioner
    KUESIONER KEPUASAN MAHASISWA 2021/2022 (SEMESTER GENAP)

--- 0029 - Igracias Cheat/script.js
+++ 0029 - Igracias Cheat/script.js
@@ -4,3 +4,11 @@ for (let value of x) {
   if (index % 4 == 0) value.setAttribute("checked", "");
   index++;
 }
+
+// Alternative
+
+let x = document.querySelectorAll(".answerlist2");
+for (let value of x) {
+  if (value.nextSibling.innerHTML == "Sangat puas") 
+    value.querySelector("input").setAttribute("checked", "");
+}
