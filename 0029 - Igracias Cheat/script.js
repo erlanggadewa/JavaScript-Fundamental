ddf4579622f5454121c9cd4fc8ab84ec3cae0697
Erlangga Dewa Sakti <erlanggadewa7@gmail.com>
2023-01-07 15:08:10 +0700

    Update script.js

--- 0029 - Igracias Cheat/script.js
+++ 0029 - Igracias Cheat/script.js
@@ -14,6 +14,11 @@ for (let value of x) {
 }
 
 // udemy all checklist
+let y = document.querySelectorAll("div.ud-btn.ud-btn-large.ud-btn-link.ud-heading-md.ud-accordion-panel-toggler")
+for(let val of y){
+    val.click()
+}
+
 let x = document.querySelectorAll("input.ud-sr-only.ud-real-toggle-input")
 for(let val of x){
     val.click()
