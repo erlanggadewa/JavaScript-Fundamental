a9a781d5adae62fcb9fcd6e350d7b817d16a79b5
Erlangga Dewa Sakti <erlanggadewa7@gmail.com>
2024-06-12 10:14:40 +0700

    Update script.js

--- 0029 - Igracias Cheat/script.js
+++ 0029 - Igracias Cheat/script.js
@@ -8,12 +8,20 @@ for (let value of x) {
 // Alternative
 
 let x = document.querySelectorAll(".answerlist2");
+let textArea = document.querySelectorAll("textarea");
+let save = document.querySelector("input.floatL3");
+for (const value of textArea) {
+  value.value = "Baik";
+}
 for (let value of x) {
   if (value.nextSibling.innerHTML == "Sangat puas")
+    value.querySelector("input").setAttribute("checked", "");
+  if (value.nextSibling.innerHTML == "Ya")
     value.querySelector("input").setAttribute("checked", "");
   if (value.nextSibling.innerHTML == "Sangat setuju")
     value.querySelector("input").setAttribute("checked", "");
 }
+save.click();
 
 // udemy all checklist
 let y = document.querySelectorAll("div.ud-btn.ud-btn-large.ud-btn-link.ud-heading-md.ud-accordion-panel-toggler")
